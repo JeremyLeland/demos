d372ac4c402e09d01670f1d6fc2c61a0beff5983
Jeremy Leland <jeremy.leland@gmail.com>
2022-04-18 21:19:01 -0700

    Make a maze from the hex triangles

--- src/CellGrid.js
+++ src/CellGrid.js
@@ -81,6 +81,79 @@ export function getHexGrid( cols, rows, size = 64, offset = 0 ) {
   return cellGrid;
 }
 
+export function getHexTriangleGrid( cols, rows, size = 64, offset = 32 ) {
+  const cellPoints = Array.from(
+    Array( cols + 1 ), ( _, col ) => Array.from(
+      Array( rows + 1 ), ( _, row ) => ( {
+        x: ( col + ( ( /*col +*/ ( row + 1 ) % 2 ) /*% 2*/ ) * 0.5 ) * size + ( Math.random() - 0.5 ) * offset,
+        y: row * size + ( Math.random() - 0.5 ) * offset,
+      } )
+    )
+  );
+
+  const cellGrid = Array.from( Array( cols * 2 ), _ => Array.from( Array( rows ).fill() ) );
+
+  for ( let row = 0; row < rows; row ++ ) {
+    for ( let col = 0; col < cols; col ++ ) {
+      cellGrid[ col * 2 ][ row ] = new Cell( [
+        cellPoints[ col ][ row ],
+        cellPoints[ col ][ row + 1 ],
+        cellPoints[ col + 1 ][ row + 1 ],
+      ] );
+      cellGrid[ col * 2 + 1 ][ row ] = new Cell( [
+        cellPoints[ col ][ row ],
+        cellPoints[ col + 1 ][ row + 1 ],
+        cellPoints[ col + 1 ][ row ],
+      ] );
+    }
+    
+    row ++;
+
+    for ( let col = 0; col < cols; col ++ ) {
+      cellGrid[ col * 2 ][ row ] = new Cell( [
+        cellPoints[ col ][ row ],
+        cellPoints[ col ][ row + 1 ],
+        cellPoints[ col + 1 ][ row ],
+      ] );
+      cellGrid[ col * 2 + 1 ][ row ] = new Cell( [
+        cellPoints[ col + 1 ][ row ],
+        cellPoints[ col ][ row + 1 ],
+        cellPoints[ col + 1 ][ row + 1 ],
+      ] );
+    }
+  }
+
+  for ( let row = 0; row < rows; row ++ ) {
+    for ( let col = 0; col < cols; col ++ ) {
+      let cell = cellGrid[ col * 2 ][ row ];
+      if ( col > 0 )    cell.edges[ 0 ].neighbor = cellGrid[ col * 2 - 1 ][ row ];
+      if ( row < rows ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 ][ row + 1 ];
+      if ( col < cols ) cell.edges[ 2 ].neighbor = cellGrid[ col * 2 + 1 ][ row ];
+
+      cell = cellGrid[ col * 2 + 1 ][ row ];
+                            cell.edges[ 0 ].neighbor = cellGrid[ col * 2 ][ row ];
+      if ( col < cols - 1 ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 + 2 ][ row ];
+      if ( row > 0 )        cell.edges[ 2 ].neighbor = cellGrid[ col * 2 + 1 ][ row - 1 ];
+    }
+    
+    row ++;
+
+    for ( let col = 0; col < cols; col ++ ) {
+      let cell = cellGrid[ col * 2 ][ row ];
+      if ( col > 0 )    cell.edges[ 0 ].neighbor = cellGrid[ col * 2 - 1 ][ row ];
+      if ( col < cols ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 + 1 ][ row ];
+      if ( row > 0 )    cell.edges[ 2 ].neighbor = cellGrid[ col * 2 ][ row - 1 ];
+
+      cell = cellGrid[ col * 2 + 1 ][ row ];
+                            cell.edges[ 0 ].neighbor = cellGrid[ col * 2 ][ row ];
+      if ( row < rows - 1 ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 + 1 ][ row + 1 ];
+      if ( col < cols - 1 ) cell.edges[ 2 ].neighbor = cellGrid[ col * 2 + 2 ][ row ];
+    }
+  }
+
+  return cellGrid;
+}
+
 // TODO: Better name for this?
 export function doMazeLink( current, previous ) {
   if ( current == null || current.edges.filter( e => e.linked ).length > 0 ) {
