2aa8f393bd24d8a5afac48e4995dad8a53f9c198
Jeremy Leland <jeremy.leland@gmail.com>
2022-05-02 09:07:09 -0700

    Create CellMap to handle collection of cells

---
+++ src/CellMap.js
@@ -0,0 +1,30 @@
+export class CellMap {
+  static fromCellGrid( cellGrid ) {
+    const cells = [];
+
+    for ( let row = 0; row < cellGrid[ 0 ].length; row ++ ) {
+      for ( let col = 0; col < cellGrid.length; col ++ ) {
+        cells.push( cellGrid[ col ][ row ] );
+      }
+    }
+
+    return new CellMap( cells );
+  }
+
+  cells = [];
+
+  constructor( cells ) {
+    this.cells = cells;
+  }
+
+  fullyLink() {
+    this.cells.forEach( cell => 
+      cell.getUnlinkedEdges().forEach( edge => cell.linkTo( edge.neighbor ) ) 
+    );
+  }
+
+  removeCell( cell ) {
+    cell.detachAll();
+    this.cells = this.cells.filter( c => c != cell );
+  }
+}
