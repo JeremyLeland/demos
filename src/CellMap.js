e6d814ea9d2a564941485a8928b5de49996f4e2d
Jeremy Leland <jeremy.leland@gmail.com>
2022-05-19 10:50:59 -0700

    Move loop-getting functionality to CellMap

--- src/CellMap.js
+++ src/CellMap.js
@@ -33,4 +33,40 @@ export class CellMap {
     this.cells.forEach( cell => edges.push( ...cell.getSolidEdges() ) );
     return edges;
   }
+
+  getLoops() {
+    const loops = [];
+
+    const unvisited = new Set( this.getSolidEdges() );
+    const visited = new Set();
+    
+    while ( unvisited.size > 0 ) {
+      const points = [];
+      let [ edge ] = unvisited;
+
+      let currentCell = edge.parent;
+      let edgeIndex = currentCell.edges.indexOf( edge );
+
+      while ( !visited.has( edge ) ) {
+        visited.add( edge );
+        unvisited.delete( edge );
+
+        points.push( edge.start );
+
+        edgeIndex = ( edgeIndex + 1 ) % currentCell.edges.length;
+        edge = currentCell.edges[ edgeIndex ];
+
+        while ( edge.linked ) {
+          edgeIndex = edge.neighbor.edges.findIndex( edge => edge.neighbor == currentCell );
+          currentCell = edge.neighbor;
+          edgeIndex = ( edgeIndex + 1 ) % currentCell.edges.length;
+          edge = currentCell.edges[ edgeIndex ];
+        }
+      }
+
+      loops.push( points );
+    }
+
+    return loops;
+  }
 }
