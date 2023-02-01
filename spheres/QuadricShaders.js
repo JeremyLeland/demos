414fbbf03a06004692009a9ebf259244751a1ddf
Jeremy Leland <jeremy.leland@gmail.com>
2023-02-01 06:22:54 -0800

    Got individual shader_quadric working again.

--- spheres/QuadricShaders.js
+++ spheres/QuadricShaders.js
@@ -1,5 +1,11 @@
-// https://mathworld.wolfram.com/QuadraticEquation.html
-const SolveQuadratic = /* glsl */ `
+export const QuadricSurfaceHit = /* glsl */ `
+  struct Quadric {
+    mat4 Q;
+    vec3 minBounds;
+    vec3 maxBounds;
+  };
+
+  // https://mathworld.wolfram.com/QuadraticEquation.html
   vec2 solveQuadratic( float a, float b, float c ) {
     float discr = b * b - 4.0 * a * c; 
 
@@ -13,24 +19,22 @@ const SolveQuadratic = /* glsl */ `
       return vec2( min( x0, x1 ), max( x0, x1 ) );
     }
   }
-`;
 
-export const QuadricSurfaceHit = /* glsl */ `
-  vec4 quadricSurfaceHit( mat4 Q, vec3 minBounds, vec3 maxBounds, vec4 C, vec4 D ) {
-    float a = dot( D, Q * D );
-    float b = dot( C, Q * D ) + dot( D, Q * C );
-    float c = dot( C, Q * C );
+  vec4 quadricSurfaceHit( Quadric q, vec4 C, vec4 D ) {
+    float a = dot( D, q.Q * D );
+    float b = dot( C, q.Q * D ) + dot( D, q.Q * C );
+    float c = dot( C, q.Q * C );
 
     vec2 times = solveQuadratic( a, b, c );
 
     vec4 fPos = C + D * times.x;
     vec4 bPos = C + D * times.y;
 
-    if ( any( lessThan( fPos.xyz, minBounds ) ) || any( greaterThan( fPos.xyz, maxBounds ) ) ) {
+    if ( any( lessThan( fPos.xyz, q.minBounds ) ) || any( greaterThan( fPos.xyz, q.maxBounds ) ) ) {
       times.x = -1.0;
     }
 
-    if ( any( lessThan( bPos.xyz, minBounds ) ) || any( greaterThan( bPos.xyz, maxBounds ) ) ) {
+    if ( any( lessThan( bPos.xyz, q.minBounds ) ) || any( greaterThan( bPos.xyz, q.maxBounds ) ) ) {
       times.y = -1.0;
     }
 
@@ -40,11 +44,11 @@ export const QuadricSurfaceHit = /* glsl */ `
 
 // See: https://www.shadertoy.com/view/7tlBW8
 export const QuadricSurfaceNormal = /* glsl */ `
-  vec4 quadricSurfaceNormal( mat4 Q, vec4 P ) {
-    float a = Q[ 0 ][ 0 ], b = Q[ 0 ][ 1 ], c = Q[ 0 ][ 2 ], d = Q[ 0 ][ 3 ],
-          e = Q[ 1 ][ 0 ], f = Q[ 1 ][ 1 ], g = Q[ 1 ][ 2 ], h = Q[ 1 ][ 3 ],
-          i = Q[ 2 ][ 0 ], j = Q[ 2 ][ 1 ], k = Q[ 2 ][ 2 ], l = Q[ 2 ][ 3 ],
-          m = Q[ 3 ][ 0 ], n = Q[ 3 ][ 1 ], o = Q[ 3 ][ 2 ];
+  vec4 quadricSurfaceNormal( Quadric q, vec4 P ) {
+    float a = q.Q[ 0 ][ 0 ], b = q.Q[ 0 ][ 1 ], c = q.Q[ 0 ][ 2 ], d = q.Q[ 0 ][ 3 ],
+          e = q.Q[ 1 ][ 0 ], f = q.Q[ 1 ][ 1 ], g = q.Q[ 1 ][ 2 ], h = q.Q[ 1 ][ 3 ],
+          i = q.Q[ 2 ][ 0 ], j = q.Q[ 2 ][ 1 ], k = q.Q[ 2 ][ 2 ], l = q.Q[ 2 ][ 3 ],
+          m = q.Q[ 3 ][ 0 ], n = q.Q[ 3 ][ 1 ], o = q.Q[ 3 ][ 2 ];
     
     float x = P.x, y = P.y, z = P.z;
 
@@ -60,31 +64,29 @@ export const QuadricSurfaceNormal = /* glsl */ `
 `;
 
 export const QuadricHit = /* glsl */ `
-  ${ SolveQuadratic }
   ${ QuadricSurfaceHit }
   ${ QuadricSurfaceNormal }
 
-  // TODO: Take the inverse matrix, etc out of this. We don't need a full object for this function, just the Q and bounds
-  //       Matrial will join the Shape definition somewhere further up the heirarchy 
+  // TODO: Does it really make any sense to separate these?
 
-  // vec4 quadricHit( Shape shape, vec4 rayPos, vec4 rayDir ) {
-  //   vec4 C = rayPos * shape.inverseMatrix;
-  //   vec4 D = rayDir * shape.inverseMatrix;
+  vec4 quadricHit( Quadric q, mat4 inverseMatrix, vec4 rayPos, vec4 rayDir ) {
+    vec4 C = rayPos * inverseMatrix;
+    vec4 D = rayDir * inverseMatrix;
 
-  //   return quadricSurfaceHit( shape.Q, shape.minBounds, shape.maxBounds, C, D );
-  // }
+    return quadricSurfaceHit( q, C, D );
+  }
 
-  // vec4 quadricNormal( Shape shape, vec3 objPos, vec4 rayDir ) {
-  //   vec4 objNorm = quadricSurfaceNormal( shape.Q, vec4( objPos, 1.0 ) );
-  //   vec4 normal = normalize( objNorm * shape.normalMatrix );
+  vec4 quadricNormal( Quadric q, vec3 objPos, mat4 normalMatrix, vec4 rayDir ) {
+    vec4 objNorm = quadricSurfaceNormal( q, vec4( objPos, 1.0 ) );
+    vec4 normal = normalize( objNorm * normalMatrix );
 
-  //   // if backside, flip normal
-  //   if ( dot( rayDir, normal ) > 0.0 ) {
-  //     normal *= -1.0;
-  //   }
+    // if backside, flip normal
+    if ( dot( rayDir, normal ) > 0.0 ) {
+      normal *= -1.0;
+    }
 
-  //   return normal;
-  // }
+    return normal;
+  }
 `;
 
 export const VertScreenPos = /* glsl */ `
