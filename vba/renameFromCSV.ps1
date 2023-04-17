b4d7e39d89a702078ac924f59eaea1b2f60906a4
Jeremy Leland <jeremy.leland@gmail.com>
2023-04-17 11:57:58 -0700

    Adding powershell script to rename folder of files based on provided CSV file

---
+++ vba/renameFromCSV.ps1
@@ -0,0 +1,21 @@
+﻿param($csvPath, $srcFolder, $renamedFolder)
+
+#$csvPath = "C:\Users\iggam\Documents\rachel\RenameFiles.csv"
+#$srcFolder = "C:\Users\iggam\Documents\rachel\All-CARA-Images-Unsorted\"
+#$renamedFolder = "C:\Users\iggam\Documents\rachel\All-CARA-renamed\"
+
+New-Item -Path $renamedFolder -ItemType Directory
+
+$csv = Import-Csv -path $csvPath
+
+foreach($line in $csv)
+{ 
+    
+    $from = Join-Path -Path $srcFolder -ChildPath $line.File_Name
+    $to = Join-Path -Path $renamedFolder -ChildPath $line.'New Name'
+
+    #preview
+    Write-Output "Copy-Item -Path $($from) -Destination $($to)"
+
+    Copy-Item -Path $from -Destination $to
+} 
