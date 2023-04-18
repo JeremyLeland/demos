f7a36aec7051d555acc4fdd6184a9cd5b3ce852c
Jeremy Leland <jeremy.leland@gmail.com>
2023-04-18 12:06:07 -0700

    Pulling out Entity and ValuesPanel functionality for now

---
+++ attract_repel/Entity.js
@@ -0,0 +1,38 @@
+export class Entity {
+  x = 0;
+  y = 0;
+  angle = 0;
+
+  dx = 0;
+  dy = 0;
+  dAngle = 0;
+
+  color = 'black';
+  drawPath = new Path2D();
+
+  constructor( values ) {
+    Object.assign( this, values );
+  }
+
+  update( dt ) {
+    this.x += this.dx * dt;
+    this.y += this.dy * dt;
+    this.angle += this.dAngle * dt;
+  }
+
+  draw( ctx ) {
+    ctx.save();
+
+    ctx.translate( this.x, this.y );
+    ctx.rotate( this.angle );
+    ctx.scale( this.size, this.size );
+
+    ctx.fillStyle = this.color;
+    ctx.fill( this.drawPath );
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1 / this.size;
+    ctx.stroke( this.drawPath );
+
+    ctx.restore();
+  }
+}
