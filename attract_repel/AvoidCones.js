0c83a508b122334a450baf8e93de0b5d31b8e72a
Jeremy Leland <jeremy.leland@gmail.com>
2023-05-04 19:15:39 -0700

    Ignore avoidCones after a certain distance. This has the effect of them "pop"-ing into existance suddenly -- should we try to phase them in slower based on distance?

--- attract_repel/AvoidCones.js
+++ attract_repel/AvoidCones.js
@@ -7,46 +7,54 @@ export class AvoidCones {
   // Use filter to remove cones that collide (while updating newCone), then add newCone?
 
   constructor( closedCones ) {
-    
-    closedCones.forEach( newCone => {
-      this.cones = this.cones.filter( other => {
-        let merge = false;
-
-        if ( Util.betweenAngles( newCone.left, other.left, other.right ) ) {
-          newCone.left = other.left;
-          merge = true;
-        }
-  
-        if ( Util.betweenAngles( newCone.right, other.left, other.right ) ) {
-          newCone.right = other.right;
-          merge = true;
-        }
-  
-        if ( Util.betweenAngles( other.left, newCone.left, newCone.right ) && 
-             Util.betweenAngles( other.right, newCone.left, newCone.right ) ) {
-          merge = true;
-        }
-
-        return !merge;
-      } );
-
-      this.cones.push( newCone );
-    } );
-
-    this.cones.sort( ( a, b ) => a.left - b.left );
-
-    const combined = [];
-
-    for ( let i = 0; i < this.cones.length; i ++ ) {
-      combined.push( this.cones[ i ] );
-      combined.push( {
-        left: this.cones[ i ].right,
-        right: this.cones[ ( i + 1 ) % this.cones.length ].left,
+    if ( closedCones.length == 0 ) {
+      this.cones.push( {
+        left: -Math.PI,
+        right: Math.PI,
         open: true,
       } );
     }
+    else { 
+      closedCones.forEach( newCone => {
+        this.cones = this.cones.filter( other => {
+          let merge = false;
 
-    this.cones = combined;
+          if ( Util.betweenAngles( newCone.left, other.left, other.right ) ) {
+            newCone.left = other.left;
+            merge = true;
+          }
+    
+          if ( Util.betweenAngles( newCone.right, other.left, other.right ) ) {
+            newCone.right = other.right;
+            merge = true;
+          }
+    
+          if ( Util.betweenAngles( other.left, newCone.left, newCone.right ) && 
+              Util.betweenAngles( other.right, newCone.left, newCone.right ) ) {
+            merge = true;
+          }
+
+          return !merge;
+        } );
+
+        this.cones.push( newCone );
+      } );
+
+      this.cones.sort( ( a, b ) => a.left - b.left );
+
+      const combined = [];
+
+      for ( let i = 0; i < this.cones.length; i ++ ) {
+        combined.push( this.cones[ i ] );
+        combined.push( {
+          left: this.cones[ i ].right,
+          right: this.cones[ ( i + 1 ) % this.cones.length ].left,
+          open: true,
+        } );
+      }
+
+      this.cones = combined;
+    }
   }
 
   getCone( angle ) {
@@ -61,13 +69,13 @@ export class AvoidCones {
     }
   }
 
-  draw( ctx ) {
+  draw( ctx, scale = 100 ) {
     ctx.save();
     
     this.cones.forEach( cone => {
       ctx.beginPath();
       ctx.moveTo( 0, 0 );
-      ctx.arc( 0, 0, 100, cone.left, cone.right );
+      ctx.arc( 0, 0, scale, cone.left, cone.right );
       ctx.closePath();
       ctx.globalAlpha = cone.open ? 0.1 : 0.2;
       ctx.fill();
