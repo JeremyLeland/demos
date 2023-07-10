617a8c24474040b117ca1940d59b39ca039ef3d3
Jeremy Leland <jeremy.leland@gmail.com>
2023-07-10 16:20:49 -0700

    Update discord list script

--- vba/discordList.ps1
+++ vba/discordList.ps1
@@ -1,7 +1,7 @@
 ﻿$dir = $args[0]
 
-ls .\$dir\*\*csv | ForEach-Object { 
-    echo "$($_.Directory.Name -split 'c' ):".trim()
-    ( Import-Csv $_ | %{ $_.ID } ) -join ', '
-    echo ""
+Get-ChildItem .\$dir\*\*csv | ForEach-Object { 
+    Write-Output "$($_.Directory.Name -split 'c' ):".trim()
+    ( Import-Csv $_ | ForEach-Object { $_.ID } ) -join ', '
+    Write-Output ""
 }
---
+++ vba/discordList2.ps1
@@ -0,0 +1,11 @@
+﻿$dir = $args[0]
+
+Get-ChildItem .\$dir\*\channel.json | ForEach-Object { 
+  $json = Get-Content $_ -Raw | ConvertFrom-Json  
+  if ( $json.guild.name -eq "August 2021 Bumpers" ) {
+    Write-Output ( $json.id + ":" )
+
+    ( Import-Csv ( $_.Directory.FullName + "\messages.csv" ) | ForEach-Object { $_.ID } ) -join ', '
+    Write-Output ""
+  }
+}
