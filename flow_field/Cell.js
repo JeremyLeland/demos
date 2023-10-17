5347fa999b1fe5a2bc9aeee5fef5348e05db3b17
Jeremy Leland <jeremy.leland@gmail.com>
2023-10-16 20:27:51 -0700

    Dusting off the old Cell and CellMap code to try some other ways of pathfinding

---
+++ flow_field/Cell.js
@@ -0,0 +1,129 @@
+import { Line } from './Line.js';
+
+export class Cell {
+  x = 0;
+  y = 0;
+  edges = [];
+
+  constructor( points ) {
+    for ( let i = 0; i < points.length; i ++ ) {
+      const a = points[ i ], b = points[ ( i + 1 ) % points.length ];
+
+      const edge = new Line( ...a, ...b );
+      edge.parent = this;
+
+      this.edges.push( edge );
+    }
+
+    this.#updateCenter();
+  }
+
+  #updateCenter() {
+    this.x = 0;
+    this.y = 0;
+
+    this.edges.forEach( edge => {
+      this.x += edge.x1;
+      this.y += edge.y1;
+    } );
+
+    this.x /= this.edges.length;
+    this.y /= this.edges.length;
+  }
+
+  contains( x, y ) {
+    return this.edges.every( edge => edge.distanceTo( x, y ) > 0 );
+  }
+
+  // isConvexEdge( index ) {
+  //   const other = this.links[ index ];
+  //   if ( other ) {
+  //     const otherIndex = other.links.findIndex( l => l == this );
+
+  //     const thisBeforeEdgeStart = this.edges.at( index - 1 ).slope.angle;
+  //     const otherAfterEdgeStart = other.edges[ ( otherIndex + 1 ) % other.edges.length ].slope.angle;
+
+  //     const otherBeforeEdgeEnd = other.edges.at( otherIndex - 1 ).slope.angle;
+  //     const thisAfterEdgeEnd = this.edges[ ( index + 1 ) % this.edges.length ].slope.angle;
+
+  //     return ( Math.PI + deltaAngle( thisBeforeEdgeStart, otherAfterEdgeStart ) < Math.PI && 
+  //              Math.PI + deltaAngle( otherBeforeEdgeEnd,  thisAfterEdgeEnd    ) < Math.PI );
+  //   }
+
+  //   return false;
+  // }
+
+  // merge( index ) {
+  //   const other = this.links[ index ];
+  //   other.links.forEach( ( otherLink, i ) => {
+  //     if ( otherLink == this ) {
+  //       const extract = ( arr ) => arr.slice( i + 1 ).concat( arr.slice( 0, i ) );
+
+  //       this.edges.splice( index, 1, ...extract( other.edges ) );
+  //       this.links.splice( index, 1, ...extract( other.links ) );
+
+  //       this.#updateCenter();
+  //     }
+  //     // Update other linked cells to point to us
+  //     else if ( otherLink ) {
+  //       const neighborLinkIndex = otherLink.links.findIndex( l => l == other );
+  //       otherLink.links[ neighborLinkIndex ] = this;
+  //     }
+  //   } );
+  // }
+  
+
+  // unlink( index ) {
+  //   const other = this.links[ index ];
+  //   if ( other ) {
+  //     const otherIndex = other.links.findIndex( link => link == this );
+  //     other.links[ otherIndex ] = null;
+  //   }
+
+  //   this.links[ index ] = null;
+  // }
+
+  linkAll() {
+    this.edges.filter( e => e.neighbor ).forEach( edge =>
+      edge.linked = edge.neighbor.linked = true
+    );
+  }
+
+  unlinkAll() {
+    this.edges.filter( e => e.neighbor ).forEach( edge =>
+      edge.linked = edge.neighbor.linked = false
+    );
+  }
+
+  draw( ctx, color = 'cyan' ) {
+    ctx.beginPath();
+    ctx.arc( this.x, this.y, 0.1, 0, Math.PI * 2 );
+    ctx.fillStyle = 'red';
+    ctx.fill();
+
+    ctx.fillStyle = color;
+
+    ctx.save();
+    ctx.globalAlpha = 0.2;
+    
+    ctx.beginPath();
+    this.edges.forEach( edge => ctx.lineTo( edge.x1, edge.y1 ) );
+    ctx.fill();
+
+    ctx.restore();
+
+    this.edges.forEach( edge => {
+      ctx.strokeStyle = color;
+      edge.draw( ctx );
+
+      if ( edge.neighbor ) {
+        ctx.beginPath();
+        ctx.moveTo( this.x, this.y );
+        ctx.lineTo( edge.neighbor.parent.x, edge.neighbor.parent.y );
+
+        ctx.strokeStyle = edge.linked ? 'green' : 'maroon';
+        ctx.stroke();
+      }
+    } );
+  }
+}
---
+++ flow_field/CellGrid.js
@@ -0,0 +1,193 @@
+import { Cell } from './Cell.js';
+
+export function getSquareGrid( cols, rows, size = 1, offset = 0 ) {
+  const cellPoints = Array.from(
+    Array( cols + 1 ), ( _, col ) => Array.from(
+      Array( rows + 1 ), ( _, row ) => ( [
+        col * size + ( Math.random() - 0.5 ) * offset,
+        row * size + ( Math.random() - 0.5 ) * offset,
+      ] )
+    )
+  );
+
+  const cellGrid = Array.from(
+    Array( cols ), ( _, col ) => Array.from(
+      Array( rows ), ( _, row ) => new Cell( [
+        cellPoints[ col ][ row ],
+        cellPoints[ col ][ row + 1 ],
+        cellPoints[ col + 1 ][ row + 1 ],
+        cellPoints[ col + 1 ][ row ],
+      ] )
+    )
+  );
+
+  const cells = [];
+
+  for ( let row = 0; row < rows; row ++ ) {
+    for ( let col = 0; col < cols; col ++ ) {
+      const cell = cellGrid[ col ][ row ];
+
+      if ( col > 0 )  cell.edges[ 0 ].neighbor = cellGrid[ col - 1 ][ row ].edges[ 2 ];
+      if ( row < rows - 1 )  cell.edges[ 1 ].neighbor = cellGrid[ col ][ row + 1 ].edges[ 3 ];
+      if ( col < cols - 1 )  cell.edges[ 2 ].neighbor = cellGrid[ col + 1 ][ row ].edges[ 0 ];
+      if ( row > 0 )  cell.edges[ 3 ].neighbor = cellGrid[ col ][ row - 1 ].edges[ 1 ];
+      
+      cells.push( cell );
+    }
+  }
+
+  return cells;
+}
+
+export function getHexGrid( cols, rows, size = 64, offset = 0 ) {
+  const cellPoints = Array.from(
+    Array( cols * 2 + 1 ), ( _, col ) => Array.from(
+      Array( rows + 2 ), ( _, row ) => ( [
+        ( col + ( ( col + ( row + 1 ) % 2 ) % 2 ) * 0.3 ) * size + ( Math.random() - 0.5 ) * offset,
+        row * 0.6 * size + ( Math.random() - 0.5 ) * offset,
+      ] )
+    )
+  );
+
+  const cellGrid = Array.from(
+    Array( cols ), ( _, col ) => Array.from(
+      Array( rows ), ( _, row ) => {
+        const c = col * 2 + ( row % 2 ), r = row;
+        return new Cell( [
+          cellPoints[ c ][ r ],
+          cellPoints[ c ][ r + 1 ],
+          cellPoints[ c ][ r + 2 ],
+          cellPoints[ c + 1 ][ r + 2 ],
+          cellPoints[ c + 1 ][ r + 1 ],
+          cellPoints[ c + 1 ][ r ],
+        ] );
+      }
+    )
+  );
+
+  const cells = [];
+
+  for ( let row = 0; row < rows; row ++ ) {
+    for ( let col = 0; col < cols; col ++ ) {
+      const cell = cellGrid[ col ][ row ];
+
+      // TODO: Make these point to edges, not cells
+      if ( col > -1 * row % 2 ) {
+        if ( row > 0 )        cell.edges[ 0 ].neighbor = cellGrid[ col - 1 * ( row - 1 ) % 2 ][ row - 1 ];
+        if ( row < rows - 1 ) cell.edges[ 1 ].neighbor = cellGrid[ col - 1 * ( row + 1 ) % 2 ][ row + 1 ];
+      }
+      if ( row < rows - 2 )   cell.edges[ 2 ].neighbor = cellGrid[ col ][ row + 2 ];
+      if ( col < cols - 1 * row % 2 ) {
+        if ( row < rows - 1 ) cell.edges[ 3 ].neighbor = cellGrid[ col + 1 * row % 2 ][ row + 1 ];
+        if ( row > 0 )        cell.edges[ 4 ].neighbor = cellGrid[ col + 1 * row % 2 ][ row - 1 ];
+      }
+      if ( row > 1 )          cell.edges[ 5 ].neighbor = cellGrid[ col ][ row - 2 ];
+
+      cells.push( cell );
+    }
+  }
+
+  return cells;
+}
+
+export function getHexTriangleGrid( cols, rows, size = 64, offset = 32 ) {
+  const cellPoints = Array.from(
+    Array( cols + 1 ), ( _, col ) => Array.from(
+      Array( rows + 1 ), ( _, row ) => ( [
+        ( col + ( ( /*col +*/ ( row + 1 ) % 2 ) /*% 2*/ ) * 0.5 ) * size + ( Math.random() - 0.5 ) * offset,
+        row * size + ( Math.random() - 0.5 ) * offset,
+      ] )
+    )
+  );
+
+  const cellGrid = Array.from( Array( cols * 2 ), _ => Array.from( Array( rows ).fill() ) );
+
+  for ( let row = 0; row < rows; row ++ ) {
+    for ( let col = 0; col < cols; col ++ ) {
+      cellGrid[ col * 2 ][ row ] = new Cell( [
+        cellPoints[ col ][ row ],
+        cellPoints[ col ][ row + 1 ],
+        cellPoints[ col + 1 ][ row + 1 ],
+      ] );
+      cellGrid[ col * 2 + 1 ][ row ] = new Cell( [
+        cellPoints[ col ][ row ],
+        cellPoints[ col + 1 ][ row + 1 ],
+        cellPoints[ col + 1 ][ row ],
+      ] );
+    }
+    
+    row ++;
+
+    for ( let col = 0; col < cols; col ++ ) {
+      cellGrid[ col * 2 ][ row ] = new Cell( [
+        cellPoints[ col ][ row ],
+        cellPoints[ col ][ row + 1 ],
+        cellPoints[ col + 1 ][ row ],
+      ] );
+      cellGrid[ col * 2 + 1 ][ row ] = new Cell( [
+        cellPoints[ col + 1 ][ row ],
+        cellPoints[ col ][ row + 1 ],
+        cellPoints[ col + 1 ][ row + 1 ],
+      ] );
+    }
+  }
+
+  const cells = [];
+
+  // TODO: Make these point to edges, not cells
+  for ( let row = 0; row < rows; row ++ ) {
+    for ( let col = 0; col < cols; col ++ ) {
+      let cell = cellGrid[ col * 2 ][ row ];
+      if ( col > 0 )    cell.edges[ 0 ].neighbor = cellGrid[ col * 2 - 1 ][ row ];
+      if ( row < rows ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 ][ row + 1 ];
+      if ( col < cols ) cell.edges[ 2 ].neighbor = cellGrid[ col * 2 + 1 ][ row ];
+
+      cells.push( cell );
+
+      cell = cellGrid[ col * 2 + 1 ][ row ];
+                            cell.edges[ 0 ].neighbor = cellGrid[ col * 2 ][ row ];
+      if ( col < cols - 1 ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 + 2 ][ row ];
+      if ( row > 0 )        cell.edges[ 2 ].neighbor = cellGrid[ col * 2 + 1 ][ row - 1 ];
+
+      cells.push( cell );
+    }
+    
+    row ++;
+
+    for ( let col = 0; col < cols; col ++ ) {
+      let cell = cellGrid[ col * 2 ][ row ];
+      if ( col > 0 )    cell.edges[ 0 ].neighbor = cellGrid[ col * 2 - 1 ][ row ];
+      if ( col < cols ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 + 1 ][ row ];
+      if ( row > 0 )    cell.edges[ 2 ].neighbor = cellGrid[ col * 2 ][ row - 1 ];
+
+      cells.push( cell );
+
+      cell = cellGrid[ col * 2 + 1 ][ row ];
+                            cell.edges[ 0 ].neighbor = cellGrid[ col * 2 ][ row ];
+      if ( row < rows - 1 ) cell.edges[ 1 ].neighbor = cellGrid[ col * 2 + 1 ][ row + 1 ];
+      if ( col < cols - 1 ) cell.edges[ 2 ].neighbor = cellGrid[ col * 2 + 2 ][ row ];
+
+      cells.push( cell );
+    }
+  }
+
+  return cells;
+}
+
+export function linkMaze( cell ) {
+  const edges = cell.edges.filter( e => e.neighbor ).sort( ( a, b ) => Math.random() > 0.5 ? 1 : -1 );
+  edges.forEach( edge => doEdge( edge ) );
+}
+
+function doEdge( edge ) {
+  const nextCell = edge.neighbor.parent;
+  if ( nextCell.edges.find( e => e.linked ) ) {
+    return;
+  }
+  else {
+    edge.linked = true;
+    edge.neighbor.linked = true;
+
+    linkMaze( nextCell );
+  }
+}
