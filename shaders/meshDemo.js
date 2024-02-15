cf1cd4d06a75289fe12e5c5a3e158045afcd3ad9
Jeremy Leland <jeremy.leland@gmail.com>
2024-02-14 23:25:37 -0800

    Varying width by t. Still messed up for some control points.

--- shaders/meshDemo.js
+++ shaders/meshDemo.js
@@ -3,7 +3,7 @@ import { OrbitControls } from '../lib/controls/OrbitControls.js';
 
 export function meshDemo( mesh, updateFunc ) {
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0x111111 );
+  scene.background = new THREE.Color( 0x333333 );
 
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
   camera.position.set( 0, 0, 5 );
