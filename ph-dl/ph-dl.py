b53bd621ff3efda4597ea25dab82a41bbd2c897e
Jeremy Leland <jeremy.leland@gmail.com>
2024-04-23 21:56:20 -0700

    Print all possible outputs in cleaned up form

--- ph-dl/ph-dl.py
+++ ph-dl/ph-dl.py
@@ -10,12 +10,14 @@ args = parser.parse_args()
 r = requests.get( args.url )
 
 title = re.findall( '"video_title":"([^"]+)"', r.text )[ 0 ].replace( '\\/', '' )
-chunklistURL = re.findall( '"defaultQuality":true.+?([^"]+m3u8[^"]+)', r.text )[ 0 ].replace( '\\', '' )
+chunklistURLs = list( map( lambda url: url.replace( '\\', '' ), re.findall( '([^"]+m3u8[^"]+)', r.text ) ) )
 
-print( 'Found chunk list URL: ' + chunklistURL )
+print( 'Found chunk list URLs: ' + str( chunklistURLs ) )
 print()
 
-ffmpegCmd = 'ffmpeg -i "' + chunklistURL + '" -codec copy "' + title + '.mp4"'
+# TODO: Pick best one
+
+ffmpegCmd = 'ffmpeg -i "' + chunklistURLs[ 0 ] + '" -codec copy "' + title + '.mp4"'
 
 print( 'Running command: ' + ffmpegCmd )
 
