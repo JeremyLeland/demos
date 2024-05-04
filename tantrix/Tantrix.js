d20c93452b57d6fba31a3587abbe7d854b8b5f3d
Jeremy Leland <jeremy.leland@gmail.com>
2024-05-03 20:54:17 -0700

    First draft of isValidMove (this is screaming for a loop to avoid duplication)

--- tantrix/Tantrix.js
+++ tantrix/Tantrix.js
@@ -74,6 +74,32 @@ for ( let i = 0; i < 6; i ++ ) {
 }
 hexagon.closePath();
 
+export function drawPiece( ctx, piece ) {
+  const x = 3 * ( piece.col + ( Math.abs( piece.row ) % 2 ) / 2 );
+  const y = Math.sin( Math.PI / 3 ) * piece.row;
+  const ang = piece.rot * Math.PI / 3;
+
+  ctx.translate( x, y );
+  ctx.rotate( ang );
+
+  drawTile( ctx, piece.id );
+
+  ctx.rotate( -ang );
+
+  ctx.font = '0.4px Arial';
+  ctx.textAlign = 'center';
+  ctx.shadowColor = 'black';
+  ctx.shadowOffsetX = 2;
+  ctx.shadowOffsetY = 2;
+
+  ctx.fillStyle = 'white';
+  ctx.fillText( `(${ piece.col }, ${ piece.row })`, 0, 0 );
+
+  ctx.shadowColor = 'transparent';
+  
+  ctx.translate( -x, -y );
+}
+
 export function drawTile( ctx, tileIndex ) {
   const colorSequence = ColorSequences[ tileIndex ];
 
@@ -110,4 +136,91 @@ export function drawTile( ctx, tileIndex ) {
       start ++;
     }
   }
+}
+
+const Direction = {
+  NE: 0,
+  SE: 1,
+  S:  2,
+  SW: 3,
+  NW: 4,
+  N:  5,
+};
+
+export function isValidMove( board, move ) {
+  const oddOffset = Math.abs( move.row ) % 2;
+
+  return board.every( m => {
+    if ( m.col == move.col && m.row == move.row ) {
+      console.log( `Existing move at ${ m.col },${ m.row }!` );
+      return false;
+    }
+
+    // NE
+    if ( m.col == move.col + oddOffset && m.row == move.row - 1 ) {
+      console.log( 'Found NE: ' + JSON.stringify( m ) );
+      const us   = ColorSequences[ move.id ][ fixRot( Direction.NE - move.rot ) ];
+      const them = ColorSequences[    m.id ][ fixRot( Direction.SW - m.rot ) ];
+      console.log( `Our NE is ${ us }, their SW is ${ them }` );
+      if ( us != them ) {
+        return false;
+      }
+    }
+    // SE
+    if ( m.col == move.col + oddOffset && m.row == move.row + 1 ) {
+      console.log( 'Found SE: ' + JSON.stringify( m ) );
+      const us   = ColorSequences[ move.id ][ fixRot( Direction.SE - move.rot ) ];
+      const them = ColorSequences[    m.id ][ fixRot( Direction.NW - m.rot ) ];
+      console.log( `Our SE is ${ us }, their NW is ${ them }` );
+      if ( us != them ) {
+        return false;
+      }
+    }
+    // S
+    if ( m.col == move.col && m.row == move.row + 2 ) {
+      console.log( 'Found S: ' + JSON.stringify( m ) );
+      const us   = ColorSequences[ move.id ][ fixRot( Direction.S - move.rot ) ];
+      const them = ColorSequences[    m.id ][ fixRot( Direction.N - m.rot ) ];
+      console.log( `Our S is ${ us }, their N is ${ them }` );
+      if ( us != them ) {
+        return false;
+      }
+    }
+    // SW
+    if ( m.col == move.col - 1 + oddOffset && m.row == move.row + 1 ) {
+      console.log( 'Found SW: ' + JSON.stringify( m ) );
+      const us   = ColorSequences[ move.id ][ fixRot( Direction.SW - move.rot ) ];
+      const them = ColorSequences[    m.id ][ fixRot( Direction.NE - m.rot ) ];
+      console.log( `Our SW is ${ us }, their NE is ${ them }` );
+      if ( us != them ) {
+        return false;
+      }
+    }
+    // NW
+    if ( m.col == move.col - 1 + oddOffset && m.row == move.row - 1 ) {
+      console.log( 'Found NW: ' + JSON.stringify( m ) );
+      const us   = ColorSequences[ move.id ][ fixRot( Direction.NW - move.rot ) ];
+      const them = ColorSequences[    m.id ][ fixRot( Direction.SE - m.rot ) ];
+      console.log( `Our NW is ${ us }, their SE is ${ them }` );
+      if ( us != them ) {
+        return false;
+      }
+    }
+    // N
+    if ( m.col == move.col && m.row == move.row - 2 ) {
+      console.log( 'Found N: ' + JSON.stringify( m ) );
+      const us   = ColorSequences[ move.id ][ fixRot( Direction.N - move.rot ) ];
+      const them = ColorSequences[    m.id ][ fixRot( Direction.S - m.rot ) ];
+      console.log( `Our N is ${ us }, their S is ${ them }` );
+      if ( us != them ) {
+        return false;
+      }
+    }
+    
+    return true;
+  } );
+}
+
+function fixRot( rot ) {
+  return rot < 0 ? rot + 6 : rot % 6;
 }
