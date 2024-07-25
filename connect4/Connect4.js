516af38000d801ec6dcaf8fc0150b2899c40db60
Jeremy Leland <jeremy.leland@gmail.com>
2024-07-24 21:51:38 -0700

    Move drawing to class

--- connect4/Connect4.js
+++ connect4/Connect4.js
@@ -1,4 +1,49 @@
+const Cols = 7, Rows = 6;
+
+const piecePath = new Path2D();
+piecePath.arc( 0, 0, 0.5, 0, Math.PI * 2 );
+
+const PieceColors = [ 'red', 'yellow' ];
+
+const BoardPath = new Path2D();
+BoardPath.rect( -0.5, -0.5, Cols, Rows );
+for ( let row = 0; row < Rows; row++ ) {
+  for ( let col = 0; col < Cols; col++ ) {
+    BoardPath.moveTo( col, row );
+    BoardPath.arc( col, row, 0.4, 0, Math.PI * 2 );
+  }
+}
+
+const BoardColor = 'tan';
+
 export class Connect4 {
-  static Cols = 7;
-  static Rows = 6;
-};
+  static newGame() {
+    return new Connect4( {
+      board: Array( Cols * Rows ).fill( 0 ),
+      history: [],
+    } );
+  }
+
+  constructor( json ) {
+    Object.assign( this, json );
+  }
+
+  draw( ctx ) {
+    for ( let row = 0; row < Rows; row++ ) {
+      for ( let col = 0; col < Cols; col++ ) {
+        const team = this.board[ col + row * Cols ];
+        if ( team > 0 ) {
+          ctx.save(); {
+            ctx.translate( col, row );
+            ctx.fillStyle = PieceColors[ team - 1 ];
+            ctx.fill( piecePath );
+          }
+          ctx.restore();
+        }
+      }
+    }
+
+    ctx.fillStyle = BoardColor;
+    ctx.fill( BoardPath, 'evenodd' );
+  }
+}
