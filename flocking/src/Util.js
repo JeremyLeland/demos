6d032087dfd21f3a57dc44477b0fe2e642de7d72
Jeremy Leland <jeremy.leland@gmail.com>
2024-11-27 21:11:41 -0800

    Attempting to handle slowing down. Need to test in isolation, since the way I'm trying to regulate speed is probably janky.

--- flocking/src/Util.js
+++ flocking/src/Util.js
@@ -31,3 +31,28 @@ export function clampAngle( angle, left, right ) {
 export function closerToZero( a, b ) {
   return b < 0 ? ( b < a && a < 0 ) : ( 0 < a && a < b );
 }
+
+//
+// Math
+//
+
+const EPSILON = 1e-6;
+
+export function solveQuadratic( A, B, C ) {
+  if ( Math.abs( A ) < EPSILON ) {
+    return -C / B;
+  }
+  else {
+    const disc = B * B - 4 * A * C;
+
+    if ( disc < 0 ) {
+      return Infinity;
+    }
+    else {
+      const t0 = ( -B - Math.sqrt( disc ) ) / ( 2 * A );
+      const t1 = ( -B + Math.sqrt( disc ) ) / ( 2 * A );
+      
+      return t0 < 0 ? t1 : t0;
+    }
+  }
+}
