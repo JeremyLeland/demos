0ee5fc9eaa107531cc81cf305407127c63820354
Jeremy Leland <jeremy.leland@gmail.com>
2025-11-06 20:40:28 -0800

    Move and resize circles with mouse, change R3 with slider

--- roads/test/arcs/arcBetweenArcs2.js
+++ roads/test/arcs/arcBetweenArcs2.js
@@ -12,6 +12,8 @@ const circles = [
   { pos: [ 2, 0 ], radius: 2.5 }
 ];
 
+let r3 = 0.5;
+
 import { Canvas } from '../../src/common/Canvas.js';
 import { Grid } from '../../src/common/Grid.js';
 import * as Intersections from '../../src/common/Intersections.js';
@@ -27,6 +29,7 @@ canvas.draw = ( ctx ) => {
   ctx.lineWidth = 0.02;
   grid.draw( ctx );
 
+  ctx.lineWidth = 0.05;
   ctx.strokeStyle = 'white';
 
   circles.forEach( circle => drawCircle( ctx, ...circle.pos, circle.radius ) );
@@ -36,8 +39,9 @@ canvas.draw = ( ctx ) => {
   // Also, should it be the same for all quadrents? Or does it look better if bigger for some, smaller for others?
   // Maybe we should try to make it so all the tangent points line up?
   //  - It'll be different streets trying to connect, though, so maybe it doesn't matter? Or will it?
-  const r3 = 0.5;
+  // const r3 = 0.5;
 
+  ctx.lineWidth = 0.02;
   ctx.strokeStyle = 'tan';
   drawCircle( ctx, ...circles[ 0 ].pos, circles[ 0 ].radius + r3 );
   drawCircle( ctx, ...circles[ 1 ].pos, circles[ 1 ].radius + r3 );
@@ -88,3 +92,68 @@ function drawPoint( ctx, x, y ) {
   ctx.arc( x, y, 0.05, 0, Math.PI * 2 );
   ctx.fill();
 }
+
+canvas.pointerMove = ( m ) => {
+  if ( m.buttons == 1 ) {
+    const closest = closestCircle( m );
+
+    if ( closest ) {
+      closest.pos[ 0 ] += m.dx;
+      closest.pos[ 1 ] += m.dy;
+
+      canvas.redraw();
+    }
+  }
+}
+
+canvas.wheelInput = ( m ) => {
+  const closest = closestCircle( m );
+
+  if ( closest ) {
+    closest.radius = Math.max( r3, closest.radius + 0.1 * Math.sign( m.wheel ) );
+    canvas.redraw();
+  }
+}
+
+function closestCircle( m ) {
+  let closest, closestDist = Infinity;
+
+  circles.forEach( circle => {
+    const dist = Math.hypot( m.x - circle.pos[ 0 ], m.y - circle.pos[ 1 ] );
+
+    if ( dist < circle.radius && dist < closestDist ) {
+      closest = circle;
+      closestDist = dist;
+    }
+  } );
+
+  return closest;
+}
+
+//
+// Slider
+//
+const r3Slider = document.createElement( 'input' );
+
+Object.assign( r3Slider.style, {
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  width: '99%',
+} );
+
+Object.assign( r3Slider, {
+  type: 'range',
+  min: 0,
+  max: 1,
+  step: 0.01,
+  value: r3,
+} );
+
+document.body.appendChild( r3Slider );
+
+r3Slider.addEventListener( 'input', _ => {
+  r3 = +r3Slider.value;
+
+  canvas.redraw();
+} );
