a8f484cb5934d68bd2043c28b113cca50d8dc38a
Jeremy Leland <jeremy.leland@gmail.com>
2025-12-26 20:31:45 -0800

    Seem to have arc vs arc working in combined function

--- roads/test/arcs/arcBetweenAny.js
+++ roads/test/arcs/arcBetweenAny.js
@@ -1,25 +1,25 @@
 const streets = {
-  line1: {
-    start: [ -4, -3 ],
-    end: [ -3, 0 ],
-  },
-  line2: {
-    start: [ -1, -4 ],
-    end: [ -4, -2 ],
-  },
+  // line1: {
+  //   start: [ -4, -3 ],
+  //   end: [ -3, 0 ],
+  // },
+  // line2: {
+  //   start: [ -1, -4 ],
+  //   end: [ -4, -2 ],
+  // },
   arc1: {
     center: [ 0, 0 ],
     radius: 3,
-    startAngle: 1,
-    endAngle: -1,
-    counterclockwise: false,
+    startAngle: -1,
+    endAngle: 1,
+    counterclockwise: true,
   },
   arc2: {
-    center: [ 2, 0 ],
+    center: [ -2, 2 ],
     radius: 2,
-    startAngle: 1,
-    endAngle: 2,
-    counterclockwise: true,
+    startAngle: 2,
+    endAngle: 0,
+    counterclockwise: false,
   },
 };
 
@@ -66,9 +66,12 @@ canvas.draw = ( ctx ) => {
 
   drawControlPoints( ctx );
   
-  streetList.forEach( A => {
-    streetList.forEach( B => {
-      if ( A != B ) {
+  const A = streets.arc1;
+  const B = streets.arc2;
+
+  // streetList.forEach( A => {
+  //   streetList.forEach( B => {
+  //     if ( A != B ) {
         const intersections = getIntersections( A, B );
 
         const colors = [ 'orange', 'pink' ];
@@ -76,14 +79,24 @@ canvas.draw = ( ctx ) => {
         intersections.forEach( ( intersection, index ) => {
           ctx.fillStyle = colors[ index ];
           drawPoint( ctx, intersection );
+
+          const arc = getArcBetween( A, B, r3, intersection );
+
+          if ( arc ) {
+            drawStreet( ctx, arc );
+          }
         } );
-      }
-    } );
-  } );
+      // }
+    // } );
+  // } );
   
   
 
   // TODO: Agnostic getArcBetween
+  //       ...or not? If I can get 4 separate functions to work for now, maybe stick with that
+  //       Don't want to bend in knots trying to generalize it...
+  //       That's a lot of similar code, though.
+  //       The general getIntersections will need to be used by Arc, maybe just put it in there?
 
 
   // const intersections = Intersections.getArcLineIntersections(
@@ -115,6 +128,205 @@ canvas.draw = ( ctx ) => {
   //     between.center[ 1 ] + Math.sin( between.endAngle ) * between.radius 
   //   );
   // } );
+}
+
+function getArcBetween( A, B, radius, intersection ) {
+  
+  const angleA = getAngle( A, intersection );
+  const angleB = getAngle( B, intersection );
+  
+  // const lineAngle = Math.atan2( A.end[ 1 ] - A.start[ 1 ], A.end[ 0 ] - A.start[ 0 ] );
+  
+  // const arcAngle = fixAngle( 
+  //   Math.atan2( 
+  //     intersection[ 1 ] - arc.center[ 1 ], 
+  //     intersection[ 0 ] - arc.center[ 0 ],
+  //   ) + ( arc.counterclockwise ? -1 : 1 ) * Math.PI / 2 
+  // );
+
+  // NEXT: Need to figure out what all this means for line vs line, arc vs arc, etc
+
+  // TODO: Should we pass some combination of A/B.counterclockwise into deltaAngle to simplify this?
+
+  const turn = deltaAngle( angleA, angleB );
+  // const dot = vec2.dot( v1, vec2.subtract( [], arc.center, intersection ) );
+
+  const s0 = ( turn < 0 ? 1 : -1 ) * ( A.counterclockwise ? -1 : 1 );
+
+  // If turn is left and we're counter clockwise, then must be coming from inside
+  // If turn is left and we're clockwise, then must be coming from outside
+  // If turn is right and we're counter clockwise, then must be coming from outside
+  // If turn is right and we're clockwise, then must be coming from inside
+  // ...right?
+
+  // const s1 =  dot > 0 ? 1 : -1;   // see if we are moving toward or away from center
+  // const s1 = turn < 0 ? ( B.counterclockwise ? -1 : 1 ) : ( B.counterclockwise ? 1 : -1 );
+
+  const s1 = ( turn > 0 ? 1 : -1 ) * ( B.counterclockwise ? 1 : -1 );
+
+  // v1 is always used with s0 and radius...
+  // const lineStart = line.slice( 0, 2 );
+  // const lineEnd = line.slice( 2, 4 );
+  // const v1 = vec2.subtract( [], lineEnd, lineStart );
+  // vec2.normalize( v1, v1 );
+
+  
+  const closestToLine = {
+    point: null,
+    sign: null,
+    dist: Infinity,
+  };
+
+  [ -1, 1 ].forEach( sign => {
+
+    const offsetA = getOffset( A, sign * s0 * radius );
+    const offsetB = getOffset( B, sign * s1 * radius );
+
+    // const offsetLine = [
+    //   line[ 0 ] + v1[ 1 ] * sign * s0 * radius,
+    //   line[ 1 ] - v1[ 0 ] * sign * s0 * radius,
+    //   line[ 2 ] + v1[ 1 ] * sign * s0 * radius,
+    //   line[ 3 ] - v1[ 0 ] * sign * s0 * radius,
+    // ];
+
+    // const offsetIntersections = Intersections.getArcLineIntersections(
+    //   ...arc.center, arc.radius + sign * s1 * radius, arc.startAngle, arc.endAngle, arc.counterclockwise,
+    //   ...offsetLine,
+    // );
+
+    const offsetIntersections = getIntersections( offsetA, offsetB );
+
+    const closestToIntersection = {
+      point: null,
+      sign: null,
+      dist: Infinity,
+    };
+
+    // We want the point closest to the start of the line that is near closestToPoint
+    offsetIntersections.forEach( testIntersection => {
+      const intersectionDist = vec2.distance( testIntersection, intersection );
+
+      if ( intersectionDist < closestToIntersection.dist ) {
+        closestToIntersection.point = testIntersection;
+        closestToIntersection.sign = sign;
+        closestToIntersection.dist = intersectionDist;
+      }
+    } );
+
+    if ( closestToIntersection.point ) {
+
+      // TODO: Maybe need arc distance here? Seems like distances are messed up with arc vs arc
+      // Or find tangent now and compare that to start point? Possibly also with arc distance?
+      // *** Can we just get atan2 of closestToLine.point and deltaAngle with A.startAngle? ***
+      // (Can't use startPoint in this case, need to branch here when finding dist)
+
+
+      const startDist = A.center ?
+        deltaAngle( 
+          A.startAngle,
+          Math.atan2( 
+            closestToIntersection.point[ 1 ] - A.center[ 1 ],
+            closestToIntersection.point[ 0 ] - A.center[ 0 ], 
+          ),
+          A.counterclockwise,
+        ) :
+        vec2.distance( closestToIntersection.point, A.start );
+      
+      console.log( 'startDist = ' + startDist );
+      
+      if ( startDist < closestToLine.dist ) {
+        closestToLine.point = closestToIntersection.point;
+        closestToLine.sign = closestToIntersection.sign;
+        closestToLine.dist = startDist;
+      }
+    }
+  } );
+
+  if ( closestToLine.point ) {
+    const tangentA = getTangent( A, closestToLine.point, closestToLine.sign * s0 * radius );
+    const tangentB = getTangent( B, closestToLine.point, closestToLine.sign * s1 * radius );
+
+    // const lineTangent = [
+    //   closestToLine.point[ 0 ] - v1[ 1 ] * closestToLine.sign * s0 * radius,
+    //   closestToLine.point[ 1 ] + v1[ 0 ] * closestToLine.sign * s0 * radius,
+    // ];
+    
+    // const arcTangent = vec2.scaleAndAdd( [], 
+    //   arc.center, 
+    //   vec2.subtract( [], closestToLine.point, arc.center ), 
+    //   arc.radius / ( arc.radius + closestToLine.sign * s1 * radius )
+    // );
+
+    const tangentVectorA = vec2.subtract( [], tangentA, closestToLine.point );
+    const tangentVectorB = vec2.subtract( [], tangentB, closestToLine.point );
+
+    return {
+      center: closestToLine.point,
+      radius: radius,
+      startAngle: Math.atan2( tangentVectorA[ 1 ], tangentVectorA[ 0 ] ),
+      endAngle: Math.atan2( tangentVectorB[ 1 ], tangentVectorB[ 0 ] ),
+      counterclockwise: closestToLine.sign * turn < 0,
+    }
+  }
+}
+
+function getAngle( A, intersection ) {
+  if ( A.center ) {
+    return fixAngle( 
+      Math.atan2( 
+        intersection[ 1 ] - A.center[ 1 ], 
+        intersection[ 0 ] - A.center[ 0 ],
+      ) + ( A.counterclockwise ? -1 : 1 ) * Math.PI / 2 
+    );
+  }
+  else {
+    return Math.atan2( 
+      A.end[ 1 ] - A.start[ 1 ], 
+      A.end[ 0 ] - A.start[ 0 ],
+    );
+  }
+}
+
+function getOffset( A, offsetDist ) {
+  if ( A.center ) {
+    const offset = structuredClone( A );
+    offset.radius += offsetDist;
+    return offset;
+  }
+  else {
+    const v1 = vec2.subtract( [], A.end, A.start );
+    vec2.normalize( v1, v1 );
+    
+    return {
+      start: [
+        line[ 0 ] + v1[ 1 ] * offsetDist,
+        line[ 1 ] - v1[ 0 ] * offsetDist,
+      ],
+      end: [
+        line[ 2 ] + v1[ 1 ] * offsetDist,
+        line[ 3 ] - v1[ 0 ] * offsetDist,
+      ],
+    };
+  }
+}
+
+function getTangent( A, point, radius ) {
+  if ( A.center ) {
+    return vec2.scaleAndAdd( [],
+      A.center, 
+      vec2.subtract( [], point, A.center ),
+      A.radius / ( A.radius + radius )
+    );
+  }
+  else {
+    const v1 = vec2.subtract( [], A.end, A.start );
+    vec2.normalize( v1, v1 );
+
+    return [
+      point[ 0 ] - v1[ 1 ] * radius,
+      point[ 1 ] + v1[ 0 ] * radius,
+    ];
+  }    
 }
 
 function getIntersections( A, B ) {
@@ -286,8 +498,8 @@ function fixAngle( a ) {
   }
 }
 
-function deltaAngle( a, b ) {
-  return fixAngle( b - a );
+function deltaAngle( a, b, counterclockwise = false ) {
+  return counterclockwise ? fixAngle( a - b ) : fixAngle( b - a );
 }
 
 //
