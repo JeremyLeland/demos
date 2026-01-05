0d2bf286e07fe51f2fbfd1087d91be78ea091353
Jeremy Leland <jeremy.leland@gmail.com>
2026-01-04 19:53:22 -0800

    Create streets now makes sane seeming lanes.

--- roads/test/editor/createStreets.js
+++ roads/test/editor/createStreets.js
@@ -8,14 +8,14 @@ const DEBUG_ARROW_WIDTH = DEBUG_ARROW_LENGTH / 2;
 
 const controlPoints = {
   A: {
-    start: [ -3, 1 ],
-    middle: [ 0, -2 ],
-    end: [ 3, 1 ],
+    start: [ -4, 1 ],
+    middle: [ 0, 0.9 ],
+    end: [ 4, 1 ],
   },
   B: {
-    start: [ 1, -3 ],
-    middle: [ -1, 0 ],
-    end: [ 1, 3 ],
+    start: [ 1, -4 ],
+    middle: [ 0.9, 0 ],
+    end: [ 1, 4 ],
   },
 };
 
@@ -23,6 +23,7 @@ const controlPoints = {
 
 import { Canvas } from '../../src/common/Canvas.js';
 import { Grid } from '../../src/common/Grid.js';
+import * as Intersections from '../../src/common/Intersections.js';
 
 import { vec2 } from '../../lib/gl-matrix.js'
 
@@ -43,7 +44,7 @@ canvas.draw = ( ctx ) => {
     streets[ name ] = arcFromThreePoints( points.start, points.middle, points.end );
 
     // TODO: Get this from controlPoints (which should be called something else)
-    streets[ name ].lanes = { left: 2, right: 1 };
+    streets[ name ].lanes = { left: 2, right: 2 };
   } );
 
   console.log( 'Streets:' );
@@ -84,7 +85,7 @@ canvas.draw = ( ctx ) => {
             counterclockwise: laneDir == 'left' ? !street.counterclockwise : !!street.counterclockwise,
 
             streetColor: street.color,
-            arrowColor: laneDir == 'left' ? 'lime' : 'red',
+            arrowColor: laneDir == 'left' ? 'green' : 'darkred',
             // parent: name,
           };
 
@@ -102,15 +103,176 @@ canvas.draw = ( ctx ) => {
   streetList.forEach( A => {
     streetList.forEach( B => {
       if ( A != B ) {
-        // const intersections = getIntersections( A, B );
+        const intersections = getIntersections( A, B );
 
-        // intersections.forEach( ( intersection, index ) => {
+        intersections.forEach( ( intersection, index ) => {
         //   const arc = getArcBetween( A, B, 1, intersection, ctx );
 
         //   if ( arc ) {
         //     drawStreet( ctx, arc );
         //   }
-        // } );
+          const pairs = [];
+          
+          const angles = [ A, B ].map( a =>
+            fixAngle( 
+              Math.atan2( 
+                intersection[ 1 ] - a.center[ 1 ], 
+                intersection[ 0 ] - a.center[ 0 ],
+              ) + ( a.counterclockwise ? -1 : 1 ) * Math.PI / 2 
+            )
+          );
+      
+          const turn = deltaAngle( ...angles );
+          // console.log( 'turn = ' + turn );
+      
+      
+          const Same = [
+            { from: 'left', to: 'left' },
+            { from: 'right', to: 'right' },
+          ];
+      
+          const Opposite = [
+            { from: 'left', to: 'right' },
+            { from: 'right', to: 'left' },
+          ];
+      
+          [
+            { from: A, to: B, lanePairs: turn < 0 ? Same : Opposite },
+            { from: B, to: A, lanePairs: turn > 0 ? Same : Opposite },
+          ].forEach( e => {
+            const outerLeft = [];
+            const localPairs = [];
+      
+            const baseRadius = 0;
+      
+            // Can create all the radii without radius, then add this at the end
+            
+            e.lanePairs.forEach( lanes => {
+              // Left
+              const leftFromLanes = e.from.lanes[ lanes.from ];
+              const leftToLanes = e.to.lanes[ lanes.to ];
+      
+              const leftLanes = Math.min( leftFromLanes, leftToLanes );
+      
+              // Left-most lane should always be able to turn left, so work left-to-right
+              // For simplicity, each from-lane can only go to one to-lane
+      
+              for ( let i = 0; i < leftLanes; i ++ ) {
+                const pair = {
+                  from: e.from.routes[ lanes.from ][ i ],
+                  to:     e.to.routes[ lanes.to   ][ i ],
+                  radius: baseRadius - LANE_WIDTH * ( leftLanes - i - 1 ),
+      
+                  // debug
+                  arrowColor: 'lime',
+                };
+      
+                if ( i == leftLanes - 1 ) {
+                  outerLeft.push( pair );
+                }
+      
+                localPairs.push( pair );
+              }
+      
+              // Right
+              // This needs to be the opposite values of above, not just switched order
+              const rightFromLanes = e.to.lanes[ lanes.to == 'right' ? 'left' : 'right' ];
+              const rightToLanes = e.from.lanes[ lanes.from == 'right' ? 'left' : 'right' ];
+      
+              const rightLanes = Math.min( rightFromLanes, rightToLanes );
+      
+              // Right-most lane should always be able to turn right, so work right-to-left
+      
+              for ( let i = 0; i < rightLanes; i ++ ) {
+                localPairs.push( {
+                  from:   e.to.routes[ lanes.to == 'right' ? 'left' : 'right' ][ rightFromLanes - i - 1 ],
+                  to:   e.from.routes[ lanes.from == 'right' ? 'left' : 'right' ][ rightToLanes - i - 1 ],
+                  radius: baseRadius - LANE_WIDTH * ( leftLanes + rightLanes - i - 1 + Math.abs( rightFromLanes - rightToLanes ) ),
+                  // radius: baseRadius - LANE_WIDTH * ( rightLanes - i ),
+                  // TODO: Is there a way to modify this radius to be better in asymetrical cases? e.g. left 1, right 2
+                  // This helps somewhat. At least its symmetrical now.
+      
+                  // debug
+                  arrowColor: 'red',
+                } );
+              }
+            } );
+      
+            let minRadius = Infinity;
+            localPairs.forEach( pair => minRadius = Math.min( minRadius, pair.radius ) );
+      
+            // console.log( `minRadius = ${ minRadius }` );
+      
+            // const radius = getRadiusForPairs( ...outerLeft, intersection );
+            const radius = Math.max( -minRadius + 1, getRadiusForPairs( ...outerLeft, intersection, routes ) );
+            // const radius = 1;
+
+            console.log( 'Radius for pairs: ' + radius );
+
+            localPairs.forEach( pair => pair.radius += radius );
+      
+            pairs.push( ...localPairs );
+          } );
+      
+      
+          // TODO: Should we try to do this at street level instead of route level?
+          //       They won't all line up nicely if there are uneven numbers of lanes
+          
+          const fromEndAngles = new Map(), toStartAngles = new Map();
+          
+          pairs.forEach( pair => {
+            const fromRoute = routes[ pair.from ];
+            const toRoute = routes[ pair.to ];
+      
+            const arc = getArcBetween( fromRoute, toRoute, pair.radius ?? 1, intersection );
+      
+            // Find min and max start and end angles for each route as we process pairs
+            const startPos = [
+              arc.center[ 0 ] + Math.cos( arc.startAngle ) * arc.radius,
+              arc.center[ 1 ] + Math.sin( arc.startAngle ) * arc.radius,
+            ];
+      
+            const endPos = [
+              arc.center[ 0 ] + Math.cos( arc.endAngle ) * arc.radius,
+              arc.center[ 1 ] + Math.sin( arc.endAngle ) * arc.radius,
+            ];
+            
+            const fromEndAngle = Math.atan2( startPos[ 1 ] - fromRoute.center[ 1 ], startPos[ 0 ] - fromRoute.center[ 0 ] );
+            const toStartAngle = Math.atan2(   endPos[ 1 ] -   toRoute.center[ 1 ],   endPos[ 0 ] -   toRoute.center[ 0 ] );
+      
+            if ( !fromEndAngles.has( pair.from ) ||
+                  isBetweenAngles( fromEndAngle, fromRoute.startAngle, fromEndAngles.get( pair.from ), fromRoute.counterclockwise ) ) {
+              fromEndAngles.set( pair.from, fromEndAngle );
+            }
+      
+            if ( !toStartAngles.has( pair.to ) ||
+                  isBetweenAngles( toStartAngle, toStartAngles.get( pair.to ), toRoute.endAngle, toRoute.counterclockwise ) ) {
+              toStartAngles.set( pair.to, toStartAngle );
+            }
+      
+            // Create route
+            arc.arrowColor = pair.arrowColor;
+      
+            const arcName = `${ pair.from }_TO_${ pair.to }_#${ index }_ARC`;
+            routes[ arcName ] = arc;
+      
+            // Keep track of our connections, and where they connect distance-wise
+            fromRoute.links ??= [];
+            fromRoute.links.push( {
+              name: arcName,
+              fromDistance: getDistanceAtAngle( fromRoute, fromEndAngle ),
+              toDistance: 0,
+            } );
+      
+            routes[ arcName ].links ??= [];
+            routes[ arcName ].links.push( {
+              name: pair.to,
+              fromDistance: getLength( routes[ arcName ] ),
+              toDistance: getDistanceAtAngle( toRoute, toStartAngle ),
+            } );
+          } );
+
+        } );
       }
     } );
   } );
@@ -154,6 +316,220 @@ function drawArrow( ctx, pos, angle, width = DEBUG_ARROW_WIDTH, length = DEBUG_A
   ctx.closePath();
   ctx.fill();
   // ctx.stroke();
+}
+
+// NOTE: This runs into issues when the arc between needs to be outside the original arcs due to large radius
+//       If the original road is truly that short, then maybe it needs to constrain the radius somehow?
+
+
+// TODO: I think what's happening is because 5 rad is too big, something else wonky is being returned.
+//       Should try to troubleshoot that more -- what are we getting back? Can we draw it? Should we be 
+//       returning null from getArcBetween instead? (may be easier to debug this in arcBetweenAny2)
+// Actually, it's saying it can't find an arc for A pair -- this is appropriate, we should be handling by going smaller
+
+function getRadiusForPairs( A, B, intersection, routes ) {
+  let left = 0, right = 10;   // TODO: How to determine appropriate max?
+
+  for ( let i = 0; i < 10; i ++ ) {
+    const mid = ( left + right ) / 2;
+
+    // console.log( 'trying radius ' + mid );
+
+    // NOW: Is getArcBetween failing if arc is out of bounds of segments?
+    //      Why are NE and SW quadrants fine, but NW quadrant is huge and SE doesn't seem to exist?
+    //      Looks like SE is generating the long way around...
+    
+    // TODO: Test if this works better with longer roads
+    // TODO: Seems like we shouldn't get the further off join if we miss the first 
+    //       one...maybe we should just fail in that case?
+    //    -  How do we detect this case? Seems like getArcBetween should 
+    //       recognize this is not keeping in sprit with the given intersection
+
+    const Aarc = getArcBetween( routes[ A.from ], routes[ A.to ], mid, intersection );
+    
+    if ( !Aarc ) {
+      console.log( 'Could not find arc for A pair!' );
+      right = mid;
+      continue;
+    }
+
+    const Barc = getArcBetween( routes[ B.from ], routes[ B.to ], mid, intersection );
+    
+    if ( !Barc ) {
+      console.log( 'Could not find arc for B pair!' );
+      right = mid;
+      continue;
+    }
+
+    const dist = vec2.distance( Aarc.center, Barc.center );
+    const rads = Aarc.radius + Barc.radius + LANE_WIDTH;
+
+    if ( dist < rads ) {
+      left = mid;
+    }
+    else {
+      right = mid;
+    }
+  }
+
+  return ( left + right ) / 2;
+}
+
+//
+// Getting arc between routes
+//
+
+function getArcBetween( A, B, radius, intersection, ctx ) {
+  const angleA = getAngle( A, intersection );
+  const angleB = getAngle( B, intersection );
+  
+  // TODO: Should we pass some combination of A/B.counterclockwise into deltaAngle to simplify this?
+  const turn = deltaAngle( angleA, angleB );
+
+  const s0 = ( turn < 0 ? 1 : -1 ) * ( A.counterclockwise ? -1 : 1 );
+
+  // If turn is left and we're counter clockwise, then must be coming from inside
+  // If turn is left and we're clockwise, then must be coming from outside
+  // If turn is right and we're counter clockwise, then must be coming from outside
+  // If turn is right and we're clockwise, then must be coming from inside
+
+  const s1 = ( turn > 0 ? 1 : -1 ) * ( B.counterclockwise ? 1 : -1 );
+  
+  const closestToLine = {
+    point: null,
+    dist: Infinity,
+  };
+
+  const offsetA = getOffset( A, s0 * radius );
+  const offsetB = getOffset( B, s1 * radius );
+
+  const offsetIntersections = getIntersections( offsetA, offsetB );
+
+  offsetIntersections.forEach( testIntersection => {
+    const angleOffsetA = getAngle( offsetA, testIntersection );
+    const angleOffsetB = getAngle( offsetB, testIntersection );
+    const turnOffset = deltaAngle( angleOffsetA, angleOffsetB );
+
+    if ( Math.sign( turn ) == Math.sign( turnOffset ) ) {
+      const startDist = A.center ?
+        deltaAngle( 
+          A.startAngle,
+          Math.atan2( 
+            testIntersection[ 1 ] - A.center[ 1 ],
+            testIntersection[ 0 ] - A.center[ 0 ], 
+          ),
+          A.counterclockwise,
+        ) :
+        vec2.distance( testIntersection, A.start );
+              
+      if ( startDist < closestToLine.dist ) {
+        closestToLine.point = testIntersection;
+        closestToLine.dist = startDist;
+      }
+    }
+  } );
+
+  if ( closestToLine.point ) {
+    const tangentA = getTangent( A, closestToLine.point, s0 * radius );
+    const tangentB = getTangent( B, closestToLine.point, s1 * radius );
+
+    const tangentVectorA = vec2.subtract( [], tangentA, closestToLine.point );
+    const tangentVectorB = vec2.subtract( [], tangentB, closestToLine.point );
+
+    return {
+      center: closestToLine.point,
+      radius: radius,
+      startAngle: Math.atan2( tangentVectorA[ 1 ], tangentVectorA[ 0 ] ),
+      endAngle: Math.atan2( tangentVectorB[ 1 ], tangentVectorB[ 0 ] ),
+      counterclockwise: turn < 0,
+    }
+  }
+}
+
+function getAngle( A, intersection ) {
+  if ( A.center ) {
+    return fixAngle( 
+      Math.atan2( 
+        intersection[ 1 ] - A.center[ 1 ], 
+        intersection[ 0 ] - A.center[ 0 ],
+      ) + ( A.counterclockwise ? -1 : 1 ) * Math.PI / 2 
+    );
+  }
+  else {
+    return Math.atan2( 
+      A.end[ 1 ] - A.start[ 1 ], 
+      A.end[ 0 ] - A.start[ 0 ],
+    );
+  }
+}
+
+function getOffset( A, offsetDist ) {
+  if ( A.center ) {
+    const offset = structuredClone( A );
+    offset.radius += offsetDist;
+    return offset;
+  }
+  else {
+    const v1 = vec2.subtract( [], A.end, A.start );
+    vec2.normalize( v1, v1 );
+    
+    return {
+      start: [
+        A.start[ 0 ] + v1[ 1 ] * offsetDist,
+        A.start[ 1 ] - v1[ 0 ] * offsetDist,
+      ],
+      end: [
+        A.end[ 0 ] + v1[ 1 ] * offsetDist,
+        A.end[ 1 ] - v1[ 0 ] * offsetDist,
+      ],
+    };
+  }
+}
+
+function getTangent( A, point, radius ) {
+  if ( A.center ) {
+    return vec2.scaleAndAdd( [],
+      A.center, 
+      vec2.subtract( [], point, A.center ),
+      A.radius / ( A.radius + radius )
+    );
+  }
+  else {
+    const v1 = vec2.subtract( [], A.end, A.start );
+    vec2.normalize( v1, v1 );
+
+    return [
+      point[ 0 ] - v1[ 1 ] * radius,
+      point[ 1 ] + v1[ 0 ] * radius,
+    ];
+  }    
+}
+
+function getIntersections( A, B ) {
+  if ( A.center && B.center ) {
+    return Intersections.getArcArcIntersections(
+      A.center[ 0 ], A.center[ 1 ], A.radius, A.startAngle, A.endAngle, A.counterclockwise,
+      B.center[ 0 ], B.center[ 1 ], B.radius, B.startAngle, B.endAngle, B.counterclockwise,
+    );
+  }
+  else if ( A.center ) {
+    return Intersections.getArcLineIntersections(
+      A.center[ 0 ], A.center[ 1 ], A.radius, A.startAngle, A.endAngle, A.counterclockwise,
+      B.start[ 0 ], B.start[ 1 ], B.end[ 0 ], B.end[ 1 ],
+    );
+  }
+  else if ( B.center ) {
+    return Intersections.getArcLineIntersections(
+      B.center[ 0 ], B.center[ 1 ], B.radius, B.startAngle, B.endAngle, B.counterclockwise,
+      A.start[ 0 ], A.start[ 1 ], A.end[ 0 ], A.end[ 1 ],
+    );
+  }
+  else {
+    return Intersections.getLineLineIntersections(
+      A.start[ 0 ], A.start[ 1 ], A.end[ 0 ], A.end[ 1 ],
+      B.start[ 0 ], B.start[ 1 ], B.end[ 0 ], B.end[ 1 ],
+    );
+  }
 }
 
 //
