da578dcdef7a546fbe79f534236ba5530291355f
Jeremy Leland <jeremy.leland@gmail.com>
2026-06-22 23:38:32 -0700

    Keep track of selected limb color. Use more constants. Draw quadrants for each limb.

--- spinner/spinner.js
+++ spinner/spinner.js
@@ -23,8 +23,13 @@ const Constants = {
   ],
 };
 
+const NumLimbs = Constants.Limbs.length;
+const NumColors = Constants.Colors.length;
+
 const TWO_PI = Math.PI * 2;
 const PI_2 = Math.PI / 2;
+
+const ColorAngleWidth = TWO_PI / NumLimbs / NumColors;
 
 const spinnerPath = new Path2D();
 spinnerPath.arc( Constants.Spinner.TipLength, 0, Constants.Spinner.TipRadius, -PI_2, PI_2 );
@@ -33,7 +38,9 @@ spinnerPath.arc( -Constants.Spinner.BaseLength, 0, Constants.Spinner.BaseRadius,
 const spinner = {
   angle: 0,
   dAngle: 0,
-}
+};
+
+const limbColors = Array( NumLimbs );
 
 const gameCanvas = new GameCanvas();
 gameCanvas.backgroundColor = 'white';
@@ -45,6 +52,14 @@ gameCanvas.update = ( dt ) => {
   if ( spinner.dAngle < 0 ) {
     spinner.dAngle = 0;
     gameCanvas.stop();
+
+    // Save resulting limb color
+    const index = Math.floor( ( spinner.angle % TWO_PI ) / ColorAngleWidth );
+
+    const limb = Math.floor( index / NumColors );
+    const color = index % NumColors;
+
+    limbColors[ limb ] = color;
   }
 }
 
@@ -107,21 +122,51 @@ gameCanvas.draw = ( ctx ) => {
   }
   ctx.restore();
 
+  const ColorOffset = 3.5;
+
+  // Quadrants
+  ctx.beginPath();
+  ctx.moveTo( ColorOffset, 0 );
+  ctx.lineTo( 5, 0 );
+  ctx.moveTo( 0, ColorOffset );
+  ctx.lineTo( 0, 5 );
+  ctx.moveTo( -ColorOffset, 0 );
+  ctx.lineTo( -5, 0 );
+  ctx.moveTo( 0, -ColorOffset );
+  ctx.lineTo( 0, -5 );
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 0.05;
+  ctx.stroke();
 
   // Colors
-  const ColorOffset = 3.5;
-  const ColorRadius = ColorOffset * TWO_PI / 32;
+  const ColorRadius = ColorOffset * ColorAngleWidth / 2;
 
-  for ( let i = 0; i < 16; i ++ ) {
+  for ( let i = 0; i < NumLimbs * NumColors; i ++ ) {
     const angle = getAngleForIndex( i );
+
     ctx.beginPath();
     ctx.arc( Math.cos( angle ) * ColorOffset, Math.sin( angle ) * ColorOffset, ColorRadius, 0, TWO_PI );
-    ctx.fillStyle = Constants.Colors[ i % 4 ];
+    ctx.fillStyle = Constants.Colors[ i % NumColors ];
     ctx.fill();
   }
 
-  ctx.rotate( spinner.angle );
+  // Selected limb color outlines
+  const OutlineWidth = 0.15;
 
+  limbColors.forEach( ( colorIndex, limbIndex ) => {
+    if ( limbColors[ limbIndex ] === colorIndex ) {
+      const angle = getAngleForIndex( limbIndex * NumColors + colorIndex );
+
+      ctx.beginPath();
+      ctx.arc( Math.cos( angle ) * ColorOffset, Math.sin( angle ) * ColorOffset, ColorRadius - OutlineWidth / 2, 0, TWO_PI );
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = OutlineWidth;
+      ctx.stroke();
+    }
+  } );
+
+  // Spinner
+  ctx.rotate( spinner.angle );
   ctx.fillStyle = 'black';
   ctx.fill( spinnerPath );
 }
@@ -137,12 +182,12 @@ const MaxSpins = 3;
 
 gameCanvas.pointerDown = ( m ) => {
 
-  const roll = Math.floor( Math.random() * 16 );
+  const roll = Math.floor( Math.random() * NumLimbs * NumColors );
 
-  const limb = Math.floor( roll / 4 );
-  const color = roll % 4;
+  const limb = Math.floor( roll / NumColors );
+  const color = roll % NumColors;
 
-  console.log( roll, Constants.Limbs[ limb ], Constants.Colors[ color ] );
+  // console.log( roll, Constants.Limbs[ limb ], Constants.Colors[ color ] );
 
   // const goalAngle = Math.atan2( m.y, m.x );
   const goalAngle = getAngleForIndex( roll );
@@ -158,5 +203,5 @@ gameCanvas.pointerDown = ( m ) => {
 }
 
 function getAngleForIndex( i ) {
-  return i * TWO_PI / 16 + TWO_PI / 32;
+  return ( i + 0.5 ) * TWO_PI / NumLimbs / NumColors;
 }
